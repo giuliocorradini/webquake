a5d17d63d400029fbc6ed26123092b0f515a567c
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2022-07-11 20:27:20 +0200

    Single graph screenshots

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import { Seismograms } from './Seismogram';
 import { useRef, useState } from 'react';
 import Header from './Header';
 import { WatsonHealthSaveImage } from '@carbon/icons-react';
-import { createRef } from 'react';
-import { useScreenshot } from 'use-react-screenshot';
-
 
 function WaitingBar(props) {
     var waiting = props.waiting;
@@ -19,6 +16,8 @@ function WaitingBar(props) {
         null;
 }
 
+var graph_images = new Array();
+
 function App() {
     const [isQueryingServer, setQueryingServer] = useState(false);
     const [isDataAvailable, setDataAvailable] = useState(false);
@@ -26,15 +25,18 @@ function App() {
     const [startDt, setStartDt] = useState(new Date());
     const [endDt, setEndDt] = useState(new Date());
 
-    const ref = createRef(null);
-    const [image, takeScreenshot] = useScreenshot();
-    const getImage = () => takeScreenshot(ref.current);
-
     function StartQuery() {
         if(!isQueryingServer) {
             setQueryingServer(true);
 
             QueryDatabase(setDataAvailable, setQueryingServer, startDt, endDt)
+        }
+    }
+
+    function saveImages() {
+        console.log(graph_images);
+        for(let i=0; i<graph_images.length; i+=2) {
+            graph_images[i]();
         }
     }
 
@@ -55,16 +57,15 @@ function App() {
                         <Column>
                             <ButtonSet>
                                 <Button renderIcon={ DocumentDownload } onClick={StartQuery}>Scarica</Button>
-                                <Button renderIcon={ WatsonHealthSaveImage } onClick={getImage}>Salva</Button>
+                                <Button renderIcon={ WatsonHealthSaveImage } onClick={saveImages} disabled={!isDataAvailable}>Salva</Button>
                                 <WaitingBar waiting={ isQueryingServer } />
                             </ButtonSet>
                         </Column>
                     </Row>
                 </FlexGrid>
-                <div ref={ref}>
-                    {isDataAvailable ? <Seismograms data={GetSeiswave()}/> : null}
+                <div>
+                    {isDataAvailable ? <Seismograms data={GetSeiswave()} imageArray={graph_images}/> : null}
                 </div>
-                <img width={400} src={image}></img>
             </Content>
         </div>
     );
--- src/Seismogram.js
+++ src/Seismogram.js
@@ -1,4 +1,9 @@
+import download from 'downloadjs';
+import { useCurrentPng } from 'recharts-to-png';
+import { useCallback } from 'react';
 import { Line, LineChart, XAxis, YAxis, CartesianGrid, Label } from "recharts";
+import { useEffect } from 'react';
+import { saveAs } from 'file-saver';
 
 function SingleSeismogram(props) {
     let time = props.time;
@@ -7,7 +12,23 @@ function SingleSeismogram(props) {
         return {v: x, t: time.add(10, 'milliseconds').format('HH:mm:ss')}
     });
 
-    return <LineChart width={1300} height={400} margin={{bottom: 90, right: 100, top: 20}} data={ data }>
+    const [getPng, {ref, isLoading}] = useCurrentPng();
+    const getImage = useCallback(async () => {
+        const png = await getPng();
+
+        console.log(props.ch);
+
+        if(png)
+            saveAs(png, `${props.ch}.png`);
+            
+    }, [getPng]);
+
+    useEffect(()=>{
+        console.log("hello effect");
+        props.imageArray.push(getImage);
+    }, []);
+
+    return <LineChart width={1300} height={400} margin={{bottom: 90, right: 100, top: 20}} data={ data } ref={ref}>
         <CartesianGrid strokeDasharray="3 3"/>
         <XAxis dataKey="t" angle={35} tickMargin={20}>
             <Label position="bottom" offset={35} value={props.ch} />
@@ -22,7 +43,7 @@ function SingleSeismogram(props) {
 export function Seismograms(props) {
 
     return props.data.map(
-        wave => <SingleSeismogram data={wave.y} key={wave.channelCode} ch={wave.channelCode} time={wave.startTime}/>
+        wave => <SingleSeismogram data={wave.y} key={wave.channelCode} ch={wave.channelCode} time={wave.startTime} imageArray={props.imageArray}/>
     );
 
 }
